0da8fbe687d888b49195ce72b24f8fa87aac1dee
StephenCOUgbana <echesteve@yahoo.com>
2021-07-07 20:36:30 +0100

    readme updated, login functions created

--- src/functions.php
+++ src/functions.php
@@ -8,5 +8,70 @@ function beneficiary_reg($fullname, $email, $password, $dob, $address, $country,
     
 }
 
+// This function logs in the donor
+function login_donor()
+{
+   // initialize session
+	session_start();
+
+    // get donor log in values
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+    $name = $_POST['name'];
+
+    // attempt  donor login
+    $query = "SELECT * FROM donors WHERE email='$email' AND password='$password'";
+	$results = mysqli_query($db, $query);
+
+    // checking if donor is found
+
+    // donor found
+    if (mysqli_num_rows($results) == 1) 
+    {   
+        $_SESSION["user"] = $name;  // set session variable to donor name
+        // redirect user to their dashboard
+        header('location: ..donors-dashboard/dashboard.html'); // redirect page needs to be corrected
+    }
+    // donor not found
+    else
+    {
+        // redirect user to signup page
+        header('location: ..log in and sign up/signup.html'); // redirect page needs to be corrected
+    }
+
+// This function logs in the beneficiary
+function login_beneficiary()
+{
+    // initialize session
+	session_start();
+
+    // get beneficiary log in values
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+    $name = $_POST['name'];
+
+    // attempt benefiiary login
+    $query = "SELECT * FROM beneficiary WHERE email='$email' AND password='$password'";
+	$results = mysqli_query($db, $query);
+
+    // checking if beneficairy is found
+
+    // beneficiary found
+    if (mysqli_num_rows($results) == 1) 
+    {   
+        $_SESSION["user"] = $name;  // set session variable to donor name
+        // redirect user to their dashboard
+        header('location: index.html'); // redirect page needs to be corrected
+    }
+    // beneficairy not found
+    else
+    {
+        // redirect user to signup page
+        header('location: ..log in and sign up/signup.html'); // redirect page needs to be corrected
+    }
+ }
+
+}
+
 
 ?>
